699f9c764a7d507ea1552286c9c583addf57b6d6
kimulchan <leerabi577@gmail.com>
2021-08-06 23:42:56 +0900

    ⚡️ :: BottomBar 퍼블리싱

---
+++ library-project/src/Routes/BottomBar.js
@@ -0,0 +1,54 @@
+import styled from 'styled-components';
+
+function BottomBar() {
+    return(
+        <>
+            <BottomBarWrapper>
+                <BottomText>    
+                    <BottomLogo>Library
+                    
+                    </BottomLogo>
+                    <Text>
+                        Made by Uichan&Bumjin
+                    </Text>
+                </BottomText>
+            </BottomBarWrapper>
+        </>
+    );
+}
+export default BottomBar;
+const BottomBarWrapper= styled.div`
+    width: 100%;
+    height:100px;
+    background-color: #eeeeee;
+    display: flex;
+    justify-content: center;
+    border-top : 1px solid #dddddd;
+    
+`
+const BottomText = styled.div`
+    width: 60%;
+    height: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`
+const BottomLogo=styled.div`
+    padding: 0 20px;
+    height: 80%;
+    font-size: 50px;
+    font-weight:600;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+`
+
+const Text = styled.div`
+    padding: 0 20px;
+    display: flex;
+    align-items:center;
+    justify-content: center;
+    height: 80%;
+    font-size: 20px;
+`
