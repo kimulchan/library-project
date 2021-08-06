279822753c7888e34a7bfca860498372383a90f4
kimulchan <leerabi577@gmail.com>
2021-08-06 23:23:27 +0900

    ⚡️ :: 글로벌 style 업데이트

---
+++ library-project/src/Styles/GlobalStyles.js
@@ -0,0 +1,32 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const GlobalStyles= createGlobalStyle`
+    ${reset}
+    *{
+        list-style: none;
+        box-sizing: border-box;
+     
+    }
+    div{
+        font-family:"Brush Script MT";
+    }
+    button{
+        display:flex;
+        align-items: center;
+        justify-content: center;
+        cursor: pointer;
+        outline:none;
+        border-radius: 3px;
+
+    };
+    input{
+        display:flex;
+        align-items: center;
+        justify-content: center;
+
+    }
+
+`
+
+export default GlobalStyles;
