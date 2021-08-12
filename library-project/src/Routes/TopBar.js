ba72c281b70b3b7c0844ad0ec8aec5be9349afd8
kimulchan <leerabi577@gmail.com>
2021-08-12 10:02:44 +0900

    ⚰️ :: 스타일 삭제

--- library-project/src/Routes/TopBar.js
+++ library-project/src/Routes/TopBar.js
@@ -1,5 +1,7 @@
-import styled from "styled-components";
+
 import { Link }from "react-router-dom"
+
+import {Menu, MenuWrapper,Logo,ContentsWrapper,Wrapper} from "../Styles/TopBar-style"
 function TopBar() {
     return(
         <Wrapper>
@@ -16,32 +18,3 @@ function TopBar() {
     );
 }
 export default TopBar;
-const Wrapper = styled.div`
-    width :100%;
-    height: 150px;
-    display: flex;
-    justify-content: center;
-    min-width: fit-content;
-    border-bottom: 1px solid #dddddd;
-`
-const ContentsWrapper = styled.div`
-    width:60%;
-    `
-const Logo = styled.div`
-    display: flex;
-    align-items: center;
-
-    height: 70%;
-    font-size: 40px;
-    font-weight: 600;
-    color: black;
-`
-const MenuWrapper = styled.div`
-    width: 50%;
-    height: 30%;
-    display: flex;
-    justify-content: space-between;
-`
-const Menu = styled.div`
-    font-size: 20px;
-`
