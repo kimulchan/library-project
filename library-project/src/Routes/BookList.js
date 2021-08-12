0f6ff75014a80381fccf9e1e1be904e32ec6225e
kimulchan <leerabi577@gmail.com>
2021-08-12 10:00:28 +0900

    ⚰️ :: 스타일 삭제

--- library-project/src/Routes/BookList.js
+++ library-project/src/Routes/BookList.js
@@ -1,6 +1,7 @@
-import styled from "styled-components";
-
+import {BookName,BookNumber,Book,List,BookTitle,ListWrapper} from '../Styles/BookList-style';
+import ShowBook from "../Components/BookList/ShowBook";
 function BookList() {
+    
     return(  
     <>  
     <BookTitle>Book List</BookTitle>
@@ -10,16 +11,9 @@ function BookList() {
                 <BookNumber>BookNumber</BookNumber>
                 <BookName>BookTitle</BookName>
                 <BookName>Library</BookName>
-                
-                
             </Book>
-            <Book>
-                <BookNumber>BookNuddmber</BookNumber>
-                <BookName>BookTiddddddddtle</BookName>
-                <BookName>Librdddary</BookName>
-                
-                
-            </Book>
+            <ShowBook></ShowBook>
+            
         </List>
     </ListWrapper>
 
@@ -28,49 +22,3 @@ function BookList() {
 }
 
 export default BookList;
-
-const BookTitle= styled.div`
-    font-size: 45px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin: 60px 0;
-`
-const ListWrapper= styled.div`
-    width: 100%;
-    display: flex;
-    justify-content: center;
-`
-
-const List = styled.div`
-    width: 60%;
-    height: calc(100vh - 415.8px);
-    
-    border-top: #dddddd solid 1px;
-    overflow: auto;
-`
-const Book = styled.div`
-    border-bottom: #dddddd solid 1px;
-    display: flex;
-    justify-content: space-evenly;
-    height: 40px;
-
-`
-
-const BookNumber = styled.div`
-    width: 100px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    font-size: 20px;
-    
-    
-`
-
-const BookName = styled.div`
-    width: 300px;
-    display:flex;
-    align-items: center;
-    font-size: 20px;
-    justify-content: center;
-`
