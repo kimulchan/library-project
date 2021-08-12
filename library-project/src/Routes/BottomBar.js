a681510ef099e9b17350c56eb915d7b06b358670
kimulchan <leerabi577@gmail.com>
2021-08-12 10:00:59 +0900

    ⚰️ :: 스타일 삭제

--- library-project/src/Routes/BottomBar.js
+++ library-project/src/Routes/BottomBar.js
@@ -1,5 +1,5 @@
-import styled from 'styled-components';
 
+import {Text,BottomText,BottomLogo,BottomBarWrapper} from '../Styles/BottomBar-style'
 function BottomBar() {
     return(
         <>
@@ -9,7 +9,7 @@ function BottomBar() {
                     
                     </BottomLogo>
                     <Text>
-                        Made by Uichan&Bumjin
+                        Made by Uichan&Beomjin
                     </Text>
                 </BottomText>
             </BottomBarWrapper>
@@ -17,38 +17,3 @@ function BottomBar() {
     );
 }
 export default BottomBar;
-const BottomBarWrapper= styled.div`
-    width: 100%;
-    height:100px;
-    background-color: #eeeeee;
-    display: flex;
-    justify-content: center;
-    border-top : 1px solid #dddddd;
-    
-`
-const BottomText = styled.div`
-    width: 60%;
-    height: 100%;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-`
-const BottomLogo=styled.div`
-    padding: 0 20px;
-    height: 80%;
-    font-size: 50px;
-    font-weight:600;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    
-`
-
-const Text = styled.div`
-    padding: 0 20px;
-    display: flex;
-    align-items:center;
-    justify-content: center;
-    height: 80%;
-    font-size: 20px;
-`
